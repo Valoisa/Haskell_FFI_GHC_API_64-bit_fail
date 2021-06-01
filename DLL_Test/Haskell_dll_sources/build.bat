4601ab6f0e4b96b8a2458ca3aad2d046d1c8f4c1
Kyrill Briantsev <kyrab@mail.ru>
2021-06-01 08:11:41 +0300

    Misc build fixes.

--- DLL_Test/Haskell_dll_sources/build.bat
+++ DLL_Test/Haskell_dll_sources/build.bat
@@ -1,7 +1,7 @@
 cd %1
 echo Building Compilation.hs...
-ghc -c -no-hs-main -O Compilation.hs -fforce-recomp
+ghc -package ghc -c -no-hs-main -O Compilation.hs -fforce-recomp
 echo Building HSCodeGen.cpp...
-ghc -optcxx-fpermissive -O -c HSCodeGen.cpp
+ghc -O -c HSCodeGen.cpp
 echo Linking library...
 ghc -package ghc -package ghc-paths -package split -package cgi -package safe-exceptions -no-hs-main -shared -g2 -o %2/HSCodeGen.dll Compilation.o HSCodeGen.o
